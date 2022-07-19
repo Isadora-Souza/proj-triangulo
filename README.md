fb8ad953dd2502fa01ab97c26061ea90042a81c7
isaahs <isaahs41@gmail.com>
2022-07-19 17:28:16 -0300

    Atualização do Projeto

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # proj-triangulo
- Ao digitar 3 ângulos, o programa define se é um triângulo Escaleno, Isósceles ou Equilátero
+ Ao digitar 3 ângulos, o programa define se é um triângulo Escaleno, Isósceles ou Equilátero e mostra um exemplo de como seria o formato do triângulo. 
--- script.js
+++ script.js
@@ -8,7 +8,8 @@ function Enviar () {
     var ladoA = Number (comp1.value);
     var ladoB = Number (comp2.value);
     var ladoC = Number (comp3.value);
-    var img = document.createElement ('img')
+
+    var img = document.createElement ('img') //createElement= cria um elemento
     img.setAttribute ('id', 'foto')
 
     if (ladoA == " " || ladoB == " " || ladoC == " ") {
@@ -16,10 +17,10 @@ function Enviar () {
 
     } else if ((ladoA  == ladoB) && (ladoB == ladoC)) {
 
-        img.setAttribute ('src', 'foto-equi.png')
+        img.setAttribute ('src', 'foto-equi.png') // setAttribute = adiciona um novo atribulto ao elemento
         res.innerHTML = `Foi detectado que é um Triangulo Equilátero `
-        document.body.style.background = "#3E7D94"
-        res.appendChild (img)
+        document.body.style.background = "#3E7D94" // muda a cor do fundo 
+        res.appendChild (img) // acrescenta o elemento que está entre parenteses
 
     }else if ( (ladoA  == ladoB) || (ladoA == ladoC) || (ladoB == ladoC)) {
 
